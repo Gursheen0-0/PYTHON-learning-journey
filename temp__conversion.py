181f0d1cc1d9306e747eb2503308a87f97467a54
Gursheen SIngh <singhgursheen915@gmail.com>
2026-09-02 22:54:03 +0530

    Update temp__conversion.py

--- temp__conversion.py
+++ temp__conversion.py
@@ -1,10 +1,10 @@
 unit=(input("Enter the temerature unit (c/f) : "))
 measurement = float(input("Enter the temperature : "))
-if unit == "c":
+if unit == "c" or unit == "C":
     measurement = (measurement * 9/5) + 32
     unit = "F"
     print(f"The temperature is {measurement} {unit}")
-elif unit == "f":
+elif unit == "f" or unit == "F":
     measurement = (measurement - 32) * 5/9
     unit = "C"
     print(f"The temperature is {measurement} {unit}")
