7b368413f76e282b86395038183ee36b84756156
Gursheen SIngh <singhgursheen915@gmail.com>
2026-09-02 14:41:17 +0530

    Update day2_if.py

--- day2_if.py
+++ day2_if.py
@@ -2,7 +2,9 @@
 #else: some other thing is true
 age=int(input("Enter your age: "))
 
-if age>=18:
+if age>19:
     print("WOW! YOU ARE AN ADULT")
+elif age<13:
+    print("you are a BABY")
 else:
-    print("you are a baby")
+    print("you are a Teenager")
