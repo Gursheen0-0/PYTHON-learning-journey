4603a0954ebfa0e19f7300251d337b57d6f9407e
Gursheen SIngh <singhgursheen915@gmail.com>
2026-09-02 14:37:54 +0530

    Add files via upload

---
+++ day2_if.py
@@ -0,0 +1,8 @@
+#if : only if something is true  
+#else: some other thing is true
+age=int(input("Enter your age: "))
+
+if age>=18:
+    print("WOW! YOU ARE AN ADULT")
+else:
+    print("you are a baby")
