475ba114ac8c9b4a63514c982d496f77849d195b
Gursheen SIngh <singhgursheen915@gmail.com>
2026-09-02 14:17:39 +0530

    Add files via upload

---
+++ mini_project_calender(10).py
@@ -0,0 +1,14 @@
+#THIS IS MY 10TH MINI PROJECT
+import calendar
+month=int(input("ENTER THE MONTH:"))
+year=int(input("ENTER THE YEAR:"))
+print("YOUR CALENDAR IS:")
+print(calendar.month(year,month))
+
+
+#for all the months in year 
+"""
+a = int(input("Enter the Year: "))
+print("THE CALENDAR IS:")
+print(calendar.calendar(a))
+"""
